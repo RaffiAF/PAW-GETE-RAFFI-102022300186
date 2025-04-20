1ba92d23e27404803b938c10659ae84ec5411e08
Raffi_494 <firdaus.raffi04@gmail.com>
2025-04-20 17:58:54 +0700

    TPModul3

---
+++ TPMODUL3_RAFFI/app/Http/Controllers/DashboardController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        // ==================2==================
+        // - Set timezone ke Asia/Jakarta
+        // - Buat variabel nama, jam, waktu
+        // - Tentukan $salam berdasarkan jam (pagi, siang, sore, malam)
+        // - Panggil fungsi getTanggal()
+        // - Kirim data ke view 'dashboard' 
+
+        date_default_timezone_set('Asia/Jakarta');
+
+        $nama = 'Raffi Akbar Firdaus';
+        $jam = date('H');
+        $waktu = date('H:i:s');
+
+        if ($jam >= 5 && $jam < 12) {
+            $salam = 'Selamat Pagi';
+        } elseif ($jam >= 12 && $jam < 15) {
+            $salam = 'Selamat Siang';
+        } elseif ($jam >= 15 && $jam < 18) {
+            $salam = 'Selamat Sore';
+        } else {
+            $salam = 'Selamat Malam';
+        }
+
+        $tanggal = $this->getTanggal();
+
+        return view('dashboard', compact('nama', 'waktu', 'salam', 'tanggal'));
+    }
+
+
+
+
+    private function getTanggal()
+    {
+        // ==================3==================
+        // - Kembalikan tanggal sekarang dalam format dd-mm-yyyy4
+
+        return date('d-m-Y');
+    }
+}
